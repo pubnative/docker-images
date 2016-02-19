82a6c443999a2d077b4b63c8bd6b8e8c8fa9ed27
Michael Nikitochkin <miry.sof@gmail.com>
2016-02-19 18:22:10 +0100

    Added static rules for indexes in PN.

--- elasticsearch-curator/docker-entrypoint.sh
+++ elasticsearch-curator/docker-entrypoint.sh
@@ -9,7 +9,18 @@ fi
 
 # Step down via gosu
 if [ "$1" = 'curator' ]; then
-	exec gosu curator bash -c "while true; do curator --host $ELASTICSEARCH_HOST delete --disk-space $MAX_SPACE indices --older-than $OLDER_THAN_IN_DAYS --time-unit=days --timestring '%Y.%m.%d'; set -e; sleep $(( 60*60*INTERVAL_IN_HOURS )); set +e; done"
+  export CURATOR_DELETE_COMMON="curator --host $ELASTICSEARCH_HOST --master-only delete"
+  export CURATOR_DELETE="${CURATOR_DELETE_COMMON} indices --time-unit=days --timestring '%Y.%m.%d'"
+
+  export CURRATOR_CMD_BY="${CURATOR_DELETE_COMMON} --disk-space $MAX_SPACE indices --older-than 7 --time-unit=days --timestring '%Y.%m.%d'"
+  export CURRATOR_CMD_BY="${CURRATOR_CMD_BY} ; ${CURATOR_DELETE} --older-than $OLDER_THAN_IN_DAYS"
+  export CURRATOR_CMD_BY="${CURRATOR_CMD_BY} ; ${CURATOR_DELETE} --older-than 3 --suffix http_requests"
+  export CURRATOR_CMD_BY="${CURRATOR_CMD_BY} ; ${CURATOR_DELETE} --older-than 3 --suffix cpa_no_fill"
+  export CURRATOR_CMD_BY="${CURRATOR_CMD_BY} ; ${CURATOR_DELETE} --older-than 3 --suffix domain_mismatch"
+  export CURRATOR_CMD_BY="${CURRATOR_CMD_BY} ; ${CURATOR_DELETE} --older-than 3 --suffix broken_token"
+  export CURRATOR_CMD_BY="${CURRATOR_CMD_BY} ; ${CURATOR_DELETE} --older-than 3 --prefix logstash-general"
+  export SLEEP="sleep $(( 60*60*$INTERVAL_IN_HOURS ))"
+  exec gosu curator bash -c "set +e; while true; do $CURRATOR_CMD_BY ; $SLEEP ; done"
 fi
 
 # As argument is not related to curator,
