1077c14528703775122085dab8756d77850128fb
Michael Nikitochkin <miry.sof@gmail.com>
2017-02-28 13:06:07 +0100

    Added build for 157 version.

--- prestodb/entrypoint.sh
+++ prestodb/entrypoint.sh
@@ -10,16 +10,9 @@ fatal() {
   export PRESTO_UUID
 }
 
-[ "x${CATALOG_URL}" = "x" ] && {
-   fatal "CATALOG_URL must be defined"
-}
-
-echo "fetching catalog of connectors:"
-wget -q -O - ${CATALOG_URL} | tar xvfz - -C /opt/presto/installation/etc
-
 export PRESTO_DISCOVERY_URI=http://${PRESTO_DISCOVERY_HOST}:${PRESTO_DISCOVERY_PORT}
 /usr/local/bin/confd -onetime -backend env
 
-cat /opt/presto/installation/etc/*
+cat /opt/presto/installation/etc/catalog/*
 
 ./bin/launcher --config=/opt/presto/installation/etc/config.properties run
