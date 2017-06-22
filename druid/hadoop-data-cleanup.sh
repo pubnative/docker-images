00a23ebf43f576e30264926b4e7f1f20598fc6dd
Denis Zhuk <detanis@gmail.com>
2017-06-22 14:43:27 +0200

    Cleanup base files for driud middle-manager

--- druid/hadoop-data-cleanup.sh
+++ druid/hadoop-data-cleanup.sh
@@ -4,4 +4,5 @@ echo "$HADOOP_LOCAL_DIR_TTL"
 if [ "$HADOOP_LOCAL_DIR_TTL" != "" ]; then
   echo 'cleaning up'
   find /tmp/hadoop-root/mapred/local/localRunner/root/jobcache -mmin +$HADOOP_LOCAL_DIR_TTL -delete
+  find /tmp/druid/base* -mmin +$HADOOP_LOCAL_DIR_TTL -delete
 fi
